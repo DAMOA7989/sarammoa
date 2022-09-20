eceb31ef0decf7e3a30c73da93bb681b0afb4738
Hansol Yoon <yoondev3434@gmail.com>
2022-09-21 01:12:29 +0900

    firebase add the service account.

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,12 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-admin.initializeApp();
+const serviceAccount = require("./config/sarammoa_service_account.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
 module.exports.functions = functions;
 module.exports.db = admin.firestore();
 module.exports.firestore = admin.firestore;
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
 
 exports.auth = require("./modules/auth");
 exports.auth.caller = require("./modules/auth.caller");
