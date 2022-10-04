6d011ad1e7c06a2b6d8bc34f47fabc403c507838
Hansol Yoon <yoondev3434@gmail.com>
2022-10-04 17:56:16 +0900

    created at section is more responsiver than before.

--- src/pages/protected/profile/information/index.js
+++ src/pages/protected/profile/information/index.js
@@ -34,28 +34,34 @@ const Information = () => {
 
     return (
         <div className="pages-protected-profile-information">
-            <div className="boxes">
-                <ul>
-                    {(__BOXES__ || []).map((box, idx) => (
-                        <li key={idx}>
-                            <div className="container">
-                                <span className="count">{box.count}</span>
-                                <span className="label">{t(box.i18nKey)}</span>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <div className="location">
-                <h3 className="title">
-                    {t("title.profile.information.location")}
-                </h3>
-                <div className="position">
-                    <GpsIcon />
-                    <span className="country">{"Republic of Korea"}</span>
+            <section className="top">
+                <div className="boxes">
+                    <ul>
+                        {(__BOXES__ || []).map((box, idx) => (
+                            <li key={idx}>
+                                <div className="container">
+                                    <span className="count">{box.count}</span>
+                                    <span className="label">
+                                        {t(box.i18nKey)}
+                                    </span>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
-            </div>
-            <div className="created-at">{`Members sice apr 17, 2022`}</div>
+                <div className="location">
+                    <h3 className="title">
+                        {t("title.profile.information.location")}
+                    </h3>
+                    <div className="position">
+                        <GpsIcon />
+                        <span className="country">{"Republic of Korea"}</span>
+                    </div>
+                </div>
+            </section>
+            <section className="bottom">
+                <div className="created-at">{`Members sice apr 17, 2022`}</div>
+            </section>
         </div>
     );
 };
