b3c48ce189a7ffed998652605e67a041521f6ea2
Hansol Yoon <yoondev3434@gmail.com>
2022-10-04 10:48:16 +0900

    story is added.

---
+++ src/components/surface/StoryCard.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { ReactComponent as CameraIcon } from "assets/images/icons/camera.svg";
+import { Skeleton } from "@mui/material";
+
+const StoryCard = ({ type, className, writer }) => {
+    const { t } = useTranslation();
+    const [loaded, setLoaded] = React.useState(false);
+
+    const imgRef = React.useRef(null);
+
+    React.useEffect(() => {
+        const newImg = new Image();
+        newImg.src = writer?.profileThumbnailUrl;
+        newImg.onload = () => {
+            setLoaded(true);
+        };
+    }, []);
+
+    return (
+        <article className={`story-card ${className}`}>
+            <div className="profile-thumbnail-outline">
+                <div className={`profile-thumbnail ${type}`}>
+                    {type === "add" ? (
+                        <CameraIcon />
+                    ) : loaded ? (
+                        <img
+                            ref={imgRef}
+                            src={writer?.profileThumbnailUrl}
+                            alt="profile thumbnail"
+                        />
+                    ) : (
+                        <Skeleton
+                            variant="rectangular"
+                            width={80}
+                            height={80}
+                        />
+                    )}
+                </div>
+            </div>
+            <div className="nickname">
+                {type === "add" ? `+ ${t("title.add")}` : writer?.nickname}
+            </div>
+        </article>
+    );
+};
+
+export default StoryCard;
--- src/pages/public/newsfeed/index.js
+++ src/pages/public/newsfeed/index.js
@@ -1,7 +1,86 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
+import StoryCard from "components/surface/StoryCard";
+
+const __STORIES__ = [
+    {
+        key: "",
+        writer: {
+            nickname: "dusa",
+            profileThumbnailUrl:
+                "https://firebasestorage.googleapis.com/v0/b/sarammoa-cc444.appspot.com/o/75PaGQeeXzM5Jzc2bThrDnRMyWL2%2FprofileThumbnail?alt=media&token=81ca7d39-3efd-41ac-bc9b-abf4222397fa",
+        },
+    },
+    {
+        key: "",
+        writer: {
+            nickname: "dusa",
+            profileThumbnailUrl:
+                "https://firebasestorage.googleapis.com/v0/b/sarammoa-cc444.appspot.com/o/HRJzhWfzdgUzxYDJGx4FRpOiQgg2%2FprofileThumbnail?alt=media&token=b6cd6b98-2c64-46c8-9e78-e4bb3bc3a7d2",
+        },
+    },
+    {
+        key: "",
+        writer: {
+            nickname: "dusa",
+            profileThumbnailUrl:
+                "https://firebasestorage.googleapis.com/v0/b/sarammoa-cc444.appspot.com/o/UZ3sfgQ7nyUIo2EEwKHcZqKHQTm1%2FprofileThumbnail?alt=media&token=73caad31-2665-4920-a3c2-d6b57adeac64",
+        },
+    },
+    {
+        key: "",
+        writer: {
+            nickname: "dumsa",
+            profileThumbnailUrl:
+                "https://firebasestorage.googleapis.com/v0/b/sarammoa-cc444.appspot.com/o/YvOiTh3xovZ7IkB9Jx2B8GvZenX2%2FprofileThumbnail?alt=media&token=19d1eca9-dfdb-4e07-a5c3-57a2535d19c1",
+        },
+    },
+    {
+        key: "",
+        writer: {
+            nickname: "dusa",
+            profileThumbnailUrl:
+                "https://firebasestorage.googleapis.com/v0/b/sarammoa-cc444.appspot.com/o/75PaGQeeXzM5Jzc2bThrDnRMyWL2%2FprofileThumbnail?alt=media&token=81ca7d39-3efd-41ac-bc9b-abf4222397fa",
+        },
+    },
+];
+
+const __CARDS__ = [
+    {
+        key: "",
+        title: "",
+        writer: {
+            profileThumbnailUrl: "",
+            nickname: "",
+        },
+    },
+];
 
 const NewsFeed = () => {
-    return <main className="pages-public-newsfeed">newsfeed</main>;
+    const { t } = useTranslation();
+
+    return (
+        <main className="pages-public-newsfeed">
+            <div className="container">
+                <div className="stories">
+                    <ul>
+                        <li className="add">
+                            <StoryCard className="add" type="add" />
+                        </li>
+                        {(__STORIES__ || []).map((story, idx) => (
+                            <li key={idx} className="normal">
+                                <StoryCard
+                                    className="normal"
+                                    writer={story.writer}
+                                />
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="cards">cards</div>
+            </div>
+        </main>
+    );
 };
 
 export default NewsFeed;
