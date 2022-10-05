4dc0fd4eaa52ab4ff88ed037a0fa3b674070fc3f
Hansol Yoon <yoondev3434@gmail.com>
2022-10-05 14:48:03 +0900

    history tab's style is done.:

---
+++ src/components/surface/ProfileHistoryCard.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { Skeleton } from "@mui/material";
+
+const ProfileHistoryCard = ({ titleImageUrl, title }) => {
+    const [loaded, setLoaded] = React.useState(false);
+    const cardRef = React.useRef(null);
+    const rippleEffectRef = React.useRef(null);
+    const timerRef = React.useRef(null);
+    const titleImageRef = React.useRef(null);
+
+    React.useEffect(() => {
+        const newImg = new Image();
+        newImg.src = titleImageUrl;
+        newImg.onload = () => {
+            setLoaded(true);
+        };
+    }, []);
+
+    React.useEffect(() => {
+        const eventHandler = (event) => {
+            event.preventDefault();
+            if (!cardRef.current.contains(event.target)) return;
+
+            const rect = cardRef.current.getBoundingClientRect();
+            const x = event.clientX - rect.left;
+            const y = event.clientY - rect.top;
+            rippleEffectRef.current.style.left = `calc(${x}px - 1.75em)`;
+            rippleEffectRef.current.style.top = `calc(${y}px - 1.75em)`;
+
+            if (timerRef.current) {
+                clearTimeout(timerRef.current);
+                timerRef.current = null;
+                rippleEffectRef.current.classList.remove("active");
+            }
+
+            setTimeout(() => {
+                rippleEffectRef.current.classList.add("active");
+                timerRef.current = setTimeout(() => {
+                    rippleEffectRef.current.classList.remove("active");
+                }, 500);
+            }, 10);
+        };
+
+        cardRef.current.addEventListener("click", eventHandler);
+
+        return () => {
+            if (!cardRef.current) return;
+            cardRef.current.removeEventListener("click", eventHandler);
+        };
+    }, []);
+
+    return (
+        <article ref={cardRef} className="profile-history-card">
+            <div ref={titleImageRef} className="title-image">
+                {loaded ? (
+                    <img src={titleImageUrl} alt="title image" />
+                ) : (
+                    <Skeleton
+                        variant="rectangular"
+                        sx={{
+                            fontSize: "0.8125em",
+                        }}
+                    />
+                )}
+            </div>
+            <div className="title">
+                {loaded ? title : <Skeleton variant="text" sx={{}} />}
+            </div>
+            <div ref={rippleEffectRef} className="ripple-effect" />
+        </article>
+    );
+};
+
+export default ProfileHistoryCard;
--- src/pages/protected/profile/history/index.js
+++ src/pages/protected/profile/history/index.js
@@ -1,7 +1,62 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
+import ProfileHistoryCard from "components/surface/ProfileHistoryCard";
+import CommonButton from "components/button/CommonButton";
+import { ReactComponent as PlusIcon } from "assets/images/icons/profile/plus.svg";
+
+const __WRITES__ = [
+    {
+        id: 1,
+        titleImageUrl:
+            "https://mir-s3-cdn-cf.behance.net/project_modules/1400/d5dd1f150493725.62fb71a11dbf8.jpg",
+        title: "Sarammoa",
+    },
+    {
+        id: 2,
+        titleImageUrl:
+            "https://ditoday.com/wp-content/uploads/2022/05/%EC%9D%B4%EB%AF%B8%EC%A7%80-%EC%9E%90%EB%A3%8C-%EB%93%9C%EB%9E%98%EA%B7%B8%EC%95%A4%EB%93%9C%EB%A1%AD%EC%9C%BC%EB%A1%9C-%EC%95%B1%EA%B3%BC-%EC%9B%B9%EC%97%90-%EB%93%A4%EC%96%B4%EA%B0%80%EB%8A%94-%EB%AA%A8%EC%85%98%EC%9D%84-%EC%82%BD%EC%9E%85%ED%95%A0-%EC%88%98-%EC%9E%88%EB%8B%A4-648x324.png",
+        title: "Sarammoa",
+    },
+    {
+        id: 3,
+        titleImageUrl:
+            "https://i.pinimg.com/736x/9f/ca/02/9fca02144c2fb125a322a61f9d2c25d2.jpg",
+        title: "Sarammoa",
+    },
+    {
+        id: 4,
+        titleImageUrl:
+            "https://mir-s3-cdn-cf.behance.net/project_modules/1400/d5dd1f150493725.62fb71a11dbf8.jpg",
+        title: "Sarammoa",
+    },
+];
 
 const History = () => {
-    return <div>history</div>;
+    const { t } = useTranslation();
+
+    return (
+        <div className="pages-protected-profile-history">
+            <section className="top">
+                <div className="writes">
+                    <ul>
+                        {(__WRITES__ || []).map((write, idx) => (
+                            <li key={idx}>
+                                <ProfileHistoryCard
+                                    titleImageUrl={write.titleImageUrl}
+                                    title={write.title}
+                                />
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </section>
+            <section className="bottom">
+                <CommonButton className="add" color="primary">
+                    <PlusIcon />
+                </CommonButton>
+            </section>
+        </div>
+    );
 };
 
 export default History;
